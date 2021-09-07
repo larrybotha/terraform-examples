cf8fb5ba2cde951829815116dc6f176142018923
Larry Botha <lawrenceguy@gmail.com>
2021-09-07 14:39:41 +0200

    update names

--- resource_droplet.tf
+++ resource_droplet.tf
@@ -1,8 +1,7 @@
-# create 2 droplets
-resource "digitalocean_droplet" "docean-terraform-example" {
-  count     = 2
+resource "digitalocean_droplet" "terraform" {
+  count     = 1
   image     = "ubuntu-20-04-x64"
-  name      = "docean-terraform-example"
+  name      = "terraform"
   region    = "lon1"
   size      = "s-1vcpu-1gb"
   user_data = file("user_data.yml")
--- resource_ssh_key.tf
+++ resource_ssh_key.tf
@@ -1,7 +1,7 @@
 # Add the public key to Digitalocean
 # This allows one to log in to droplets without specifying the private key with
 # $ ssh user@remote -i my-key
-resource "digitalocean_ssh_key" "docean-terraform-example" {
-  name       = "docean-terraform-example"
+resource "digitalocean_ssh_key" "terraform" {
+  name       = "terraform"
   public_key = file("my-key.pub")
 }
