22a0ec113ae83021cae216a6cd68973aaf100d2f
Larry Botha <lawrenceguy@gmail.com>
2021-09-07 15:39:32 +0200

    use indexes for droplet names

--- resource_droplet.tf
+++ resource_droplet.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_droplet" "terraform" {
   count     = var.droplet.count
   image     = var.droplet.image
-  name      = var.droplet.name
+  name      = "${var.droplet.name}.${count.index}"
   region    = var.droplet.region
   size      = var.droplet.size
   user_data = file(var.droplet.user_data_file)
