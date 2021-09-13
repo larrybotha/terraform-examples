5f20c57bcccad8354b1e1d81ca52dd6e6b4d9627
Larry Botha <lawrenceguy@gmail.com>
2021-09-13 16:55:40 +0200

    don't bother with Packer example for now

--- blue-green-deployment-packer/terraform/deployment/resource_ssh_key.tf
+++
@@ -1,7 +0,0 @@
-# Add the public key to Digitalocean
-# This allows one to log in to droplets without specifying the private key with
-# $ ssh user@remote -i my-key
-resource "digitalocean_ssh_key" "terraform" {
-  name       = "ssh-key-${local.name_suffix}"
-  public_key = file(var.ssh_key.public_key_file)
-}
