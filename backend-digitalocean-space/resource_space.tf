9a99a1d6dd1e58c146944d4c4d478408cfbc0a76
Larry Botha <lawrenceguy@gmail.com>
2021-09-14 11:18:29 +0200

    update space config

--- backend-digitalocean-space/resource_space.tf
+++ backend-digitalocean-space/resource_space.tf
@@ -1,4 +1,11 @@
 resource "digitalocean_spaces_bucket" "terraform-state" {
-  name   = local.space_name
+  name   = var.space.name
   region = var.space.region
+
+  versioning {
+    enabled = true
+  }
+
+  # Destroy the bucket, even if it contains objects
+  force_destroy = true
 }
