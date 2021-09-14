343b9fd85b977b86970503f35db80adadecdea02
Larry Botha <lawrenceguy@gmail.com>
2021-09-14 11:18:44 +0200

    add remote backend

--- backend-digitalocean-space/main.tf
+++ backend-digitalocean-space/main.tf
@@ -8,10 +8,16 @@ terraform {
     }
   }
 
-  backend "s3" {}
-}
-
-# resources
-locals {
-  space_name = var.project_name
+  # Values specific to your backend should be defined in a tfvars file, and
+  # Terraform should be explicitly initialised with that file. e.g.:
+  #
+  # $ cp backend.gitignore.tfvars{.example,}
+  # $ terraform init -backend-config=backend.gitignore.tfvars
+  backend "s3" {
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    # Due to the above flags, this value is not used, but it is still a
+    # required field.  It should be set to be a valid S3 region
+    region = "us-east-1"
+  }
 }
