9d87e8ac0741817d518a15a3841937f2a0b7153e
Larry Botha <lawrenceguy@gmail.com>
2021-09-14 10:52:44 +0200

    add example tfvars, update variables

--- backend-digitalocean-space/variables.tf
+++ backend-digitalocean-space/variables.tf
@@ -1,20 +1,52 @@
 variable "project_name" {
+  default     = "terraform-example"
   description = "The project name"
   type        = string
-  default     = "terraform-example"
 }
 
 variable "environment" {
+  default     = "staging"
   description = "The project environment"
   type        = string
-  default     = "staging"
 }
 
 variable "do_access_token" {
   description = "Digitalocean personal access token"
+  type        = string
 
   validation {
     condition     = length(var.do_access_token) > 0
     error_message = "A Digitalocean access token is required."
   }
+}
+
+variable "do_spaces_access_id" {
+  description = "Digitalocean spaces access ID"
+  type        = string
+
+  validation {
+    condition     = length(var.do_spaces_access_id) > 0
+    error_message = "A Digitalocean spaces access ID is required."
+  }
+}
+
+variable "do_spaces_secret_key" {
+  description = "Digitalocean spaces secret key"
+  type        = string
+
+  validation {
+    condition     = length(var.do_spaces_secret_key) > 0
+    error_message = "A Digitalocean spaces secret key is required."
+  }
+}
+
+variable "space" {
+  default = {
+    region = "fra1"
+    name   = "terraform-example-1"
+  }
+  type = object({
+    region = string
+    name   = string
+  })
 }
