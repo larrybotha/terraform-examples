5659310819d518377ba08be05312412ab5b6e2d8
Larry Botha <lawrenceguy@gmail.com>
2021-09-22 14:35:43 +0200

    allow for dynamic certificate name without generating new certificates every time

--- blue-green-deployment/resource_certificate.tf
+++ blue-green-deployment/resource_certificate.tf
@@ -1,10 +1,11 @@
 resource "digitalocean_certificate" "loadbalancer" {
-  name             = "lb-cert-${local.name_suffix}"
+  name             = "lb-cert-${local.name_suffix}-${formatdate("YYYYMMDD-hhmmss", timestamp())}"
   type             = "custom"
   private_key      = file("${local.certs_dir}/lb.key")
   leaf_certificate = file("${local.certs_dir}/lb.crt")
 
   lifecycle {
     create_before_destroy = true
+    ignore_changes        = [name]
   }
 }
