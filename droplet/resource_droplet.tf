a87bde6f974798f7be6480a63598585dbac8dabf
Larry Botha <lawrenceguy@gmail.com>
2022-06-06 13:30:23 -0500

    chore(droplet example): remove legacy files

--- droplet/resource_droplet.tf
+++
@@ -1,9 +0,0 @@
-resource "digitalocean_droplet" "terraform" {
-  count  = var.droplet.count
-  image  = var.droplet.image
-  name   = "${var.droplet.name}.${count.index}"
-  region = var.droplet.region
-  size   = var.droplet.size
-
-  user_data = data.cloudinit_config.config.rendered
-}
