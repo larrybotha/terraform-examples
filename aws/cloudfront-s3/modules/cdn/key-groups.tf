7925a1a1c827b5c159ce99628c050a14f2e09dc2
Larry Botha <lawrenceguy@gmail.com>
2022-06-07 17:17:54 -0500

    fix(cdn/key-groups.tf): only destroy key group and public key after creating new resources

--- aws/cloudfront-s3/modules/cdn/key-groups.tf
+++ aws/cloudfront-s3/modules/cdn/key-groups.tf
@@ -5,9 +5,13 @@ resource "random_id" "key_group" {
 resource "aws_cloudfront_public_key" "cdn" {
   name        = replace("${var.distribution_name}-${random_id.key_group.hex}", ".", "-")
   encoded_key = var.cloudfront_public_key
+
+  lifecycle { create_before_destroy = true }
 }
 
 resource "aws_cloudfront_key_group" "cdn" {
   items = [aws_cloudfront_public_key.cdn.id]
   name  = "${aws_cloudfront_public_key.cdn.name}-group"
+
+  lifecycle { create_before_destroy = true }
 }
