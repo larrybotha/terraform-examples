4389c94332a06422d5a3e1add4a965f8762eb91a
Larry Botha <lawrenceguy@gmail.com>
2022-06-07 17:26:55 -0500

    chore(gen-keys.sh): echo created files

--- aws/cloudfront-s3/scripts/gen-keys.sh
+++ aws/cloudfront-s3/scripts/gen-keys.sh
@@ -8,12 +8,16 @@
 #   ./gen-keys.sh.sh
 
 function __main() {
-  local dir=./
+  local dir=.
   local private_file="${dir}/my-key.cloudfront_private.pem"
   local public_file="${dir}/my-key.cloudfront_public.pem"
 
   openssl genrsa -out "${private_file}" 2048
   openssl rsa -pubout -in "${private_file}" -out "${public_file}"
+
+  echo -e "\ngenerated the following files:"
+  echo $private_file
+  echo $public_file
 }
 
 __main "$*"
