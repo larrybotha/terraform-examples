d7d9b2060f29e8b4a686c1da9125839ac2466692
Larry Botha <lawrenceguy@gmail.com>
2022-06-07 17:10:48 -0500

    refactor(s3-bucket module): no need for module

--- aws/cloudfront-s3/modules/s3-bucket/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "ARN of the bucket"
-  value       = aws_s3_bucket.bucket.arn
-}
-
-output "id" {
-  description = "ID of the bucket"
-  value       = aws_s3_bucket.bucket.id
-}
